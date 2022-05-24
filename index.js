f49a6ca833163dccad5611a92b2b73d53a7952db
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-24 15:04:50 +0600

    make admin api

--- index.js
+++ index.js
@@ -122,6 +122,28 @@ async function run() {
 
         });
 
+        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+
+            const email = req.params.email;
+
+
+
+            const filter = { email: email };
+
+            const updateDoc = {
+                $set: { role: 'admin' },
+            };
+
+            const result = await userCollection.updateOne(filter, updateDoc);
+
+            res.send(result);
+
+
+
+
+
+        });
+
     }
     finally {
 
