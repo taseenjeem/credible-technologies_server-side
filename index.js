be588e1cc4d1c07da0e1b7bb3969edd23f2884e6
Md. Ta-Seen Fuad Jeem <mdtaseenfuad@gmail.com>
2022-05-25 14:01:40 +0600

    Added update api

--- index.js
+++ index.js
@@ -211,6 +211,25 @@ async function run() {
 
         });
 
+        app.put('/update-a-product/:id', async (req, res) => {
+
+            const id = req.params.id;
+
+            const data = req.body;
+
+            const filter = { _id: ObjectId(id) };
+
+            const options = { upsert: true };
+
+            const updatedDoc = {
+                $set: data
+            };
+
+            const result = await productCollection.updateOne(filter, updatedDoc, options);
+
+            res.send(result)
+        })
+
     }
     finally {
 
